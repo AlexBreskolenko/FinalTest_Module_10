5e16919a037d6ec9451db82affddb39fe191d242
Aleksandr <abreskolenko@yandex.ru>
2023-06-06 20:39:26 +0300

    Task 1 completed

---
+++ Task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace FinalTestModule_10
+{
+    //Определяем интерфейс
+    public interface ISum
+    {
+        int Sum(int a, int b);
+    }
+
+    class Calculate : ISum
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+
+    class Program
+    {
+        static int Messeng()
+        {
+            Console.Write("Введите число : ");
+            int num = int.Parse(Console.ReadLine());
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = Messeng();
+                int b = Messeng();
+
+                Calculate calculate = new Calculate();
+                Console.WriteLine($"{a} + {b} = {calculate.Sum(a, b)}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели не цифру !");
+            }
+        }
+    }
+}
+
